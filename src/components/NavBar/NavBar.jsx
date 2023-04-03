5f738bd567727a22e51b933ee2125a1c0bb49d1b
danielgof <gofmandon@gmail.com>
2023-04-03 14:37:53 -0400

    one style for cards

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,12 +1,21 @@
 import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import logo from './logo.png'
 
 const NavBar = () => {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="/">Main Page</Navbar.Brand>
+        <Navbar.Brand href="/">
+          <img 
+              src={logo}
+              height="45"
+              width="45"
+              className="d-inline-block align-top"
+              alt="Logo"
+            />
+          </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
