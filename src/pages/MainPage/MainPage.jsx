b09d5919ec452befab8ec727ae2151d11e5e9904
danielgof <gofmandon@gmail.com>
2023-04-04 15:21:34 -0400

    upd content on main page

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -9,15 +9,13 @@ const MainPage = () => {
         <h2>Can advertisement be benefitial for both: customer and seller?</h2>
         <br></br>
           <p className='home'>
-            My project focuses on problem of online advertisement affecting customers’ behavior and equality of
-            benefits for customers and buyer affected by advertisement, which is of interest to the community because
-            of the importance of online technologies and way they grow these days and the fact that it is affect almost
-            all area of human activity including advertisement.
+            Nowadays technologies grow fast and more and more things are adapting to new format. Advertisement is not an exclusion from that list. Growth of technologies helps sellers to develop new formats to attract customer for their product. However they not always do the way that it’ll benefit customer equally as seller. These kind of advertisement meant to sell product to customer no matter weather product is going to be used by customer or not. In that case customer only lose money. Those are examples of fraud advertisement that must be avoided. Unfortunately, it can be difficult to detect fraud advertisement. This might happened due to a lack of attention from customer to important details before purchase any goods or services. This way of shopping might result a useless purchase that is not going to benefit a customer in any way. Thus it is very important to pay attention when planning to make a purchase. 
           </p>
           <p className='home'> 
-            The purpose of my project is clear. To draw attention to the fact that advertisement in each of its form not
-            always benefit customer and seller equally, which will be accomplished by focusing separately on
-            customers’ affection by online advertisement and equality of profit for customers affect by advertisement.
+            The idea of that exhibition is to show that there are types of advertisement which can benefit customer equally as a seller. In terms of key aspect of good advertisement, it tells customer about real opportunity, not attract in order to get profit. This is the kind of advertisement that can be separated only by customer. In order to show how advertisement can be beneficial for customer was created that project. Main idea here is that examples provided in that example are showing profitable kind advertisement that are going to help customer in some way and it would not a wasted of money. Hopefully, after reviewing that can exhibition it will become clear which aspects they should pay attention when considering a purchase. To get as much benefit as you can try to pay attention to the details of each object in that exhibition. Try to think what makes each of them useful for customer. Hopefully, that exhibition will benefit you!
+          </p>
+          <p className='home'> 
+            There are numerous types of advertisement nowadays. Some of them took place online, some of it can be found on the billboards on the streets others are placed on the walls. But no matter what format does the advert has, we still witness it in large amounts. In order to protect ourselves from losing money on useless goods or services it is necessary to pay more attention when considering a transaction. However, among many fraud advertisement there are good one. These kinds of advertisement benefit both client and seller, in other words they meant to tell about goods or services not to fool a client.
           </p>
         <a href="/content">
           Click to learn more
