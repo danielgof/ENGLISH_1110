0f33fdb3a9419474b27c06cd3e619a131db6019d
danielgof <gofmandon@gmail.com>
2023-04-09 19:55:55 -0400

    add info about loyalty programs

--- src/pages/ContentPage/ObjectsPages/LoyaltyPrograms/LoyaltyPrograms.jsx
+++ src/pages/ContentPage/ObjectsPages/LoyaltyPrograms/LoyaltyPrograms.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 const LoyaltyPrograms = () => {
   return (
-    <div>
-      Info will be availble soon.
+    <div className='projects' align="center">
+      <br></br>
+      <div className='info'>
+        <br></br>
+          <p className='home'>
+            The number of loyalty programs among customer has grown among companies. This all due to the fact that these type of program turned out to be extremely profitable for companies which provide them to their customers. But such high demand among customer was caused due to the fact that these types of programs are also help clients of company gain profit from using company’s product. One good example of such program is “Starbucks Rewards”. This is program offers additional bonuses for customers who attend “Starbucks” regularly in form of free products. The bonuses can vary from client status. The idea of that program is following: by making a purchase at Starbucks a customer can earn specific points that can be later applied to receive company’s products for free. Depending on how many points (they are called stars) are received might be provided different options. More stars meaning better choice of products to get for free. It is worth to mention that in order for customer to receive points for purchase he or she must be registered a Starbucks account. The statuses vary depending on purchases that are made, so more client is purchasing more he or she can benefit from company and its products. This policy helps company attract new customers and thus grow as business and benefit customers as well. This is a good example of harmonic policy that provide benefits for both customer and seller. Additionally, this business strategy of mutual benefits also help Starbucks maintain profitable business across the world. However, one might suggest that this is beneficial only for the customers who are purchasing Starbucks’s products on a regular basis. But we should remember that this specific advertisement is aimed for regular customer. In the end, Starbucks Rewards seems to be a nice choice for ones who are fans of coffee or Starbucks’s products specifically. 
+          </p>
+          <br></br>
+      </div>
+      <br></br>
     </div>
   )
 }
